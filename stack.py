ac8817141b043b0974d61c1926956176debadb89
Noppawat Tantisiriwat <noppawat.tan@gmail.com>
2022-02-08 16:13:44 +0700

    add stack.py

---
+++ stack.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Stack:
+
+    def __init__(self, function_list: List, name="calc1"):
+
+        self.functions = function_list
+        self.name = name
+
+    def calculate(self, input_shape):
+
+        x = input_shape
+
+        for function in self.functions:
+            x = function.forward(x)
+
+        return x
+
+    def __str__(self):
+
+        return self.name + "\n ____________ " + "layers : " + str(len(self.functions)) 
+
+        
